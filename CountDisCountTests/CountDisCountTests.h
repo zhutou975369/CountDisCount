7aeacda1a7e707852b06141ad757723249478b51
zhutou975369 <guanhr8711@yahoo.cn>
2012-04-06 23:39:41 -0400

    initial commit

---
+++ CountDisCountTests/CountDisCountTests.h
@@ -0,0 +1,13 @@
+//
+//  CountDisCountTests.h
+//  CountDisCountTests
+//
+//  Created by Haoran Guan on 4/6/12.
+//  Copyright (c) 2012 NYU. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CountDisCountTests : SenTestCase
+
+@end
---
+++ CountDisCountTests/CountDisCountTests.m
@@ -0,0 +1,32 @@
+//
+//  CountDisCountTests.m
+//  CountDisCountTests
+//
+//  Created by Haoran Guan on 4/6/12.
+//  Copyright (c) 2012 NYU. All rights reserved.
+//
+
+#import "CountDisCountTests.h"
+
+@implementation CountDisCountTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CountDisCountTests");
+}
+
+@end
